ec5bb75b3ed06424e3a92e98dd218e332899c75a
Erik Tallang <e.tallang@gmail.com>
2019-05-01 16:10:54 +0200

    feat(flexbox): add content

--- src/app/flexbox/flexbox.component.ts
+++ src/app/flexbox/flexbox.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+
+import { AnchorElementDirective } from '../navigation/anchor-element.directive';
+import { NavigationManager } from '../navigation/navigation-manager';
 
 @Component({
   selector: 'app-flexbox',
   templateUrl: './flexbox.component.html',
   styleUrls: ['./flexbox.component.css']
 })
-export class FlexboxComponent implements OnInit {
+export class FlexboxComponent implements AfterViewInit {
+  @ViewChildren(AnchorElementDirective) elements: QueryList<AnchorElementDirective>;
+  constructor(private navigationManager: NavigationManager) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.navigationManager.setAnchorItems(this.elements);
   }
-
 }
--- src/app/flexbox/flexbox.module.ts
+++ src/app/flexbox/flexbox.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { FlexboxRoutingModule } from './flexbox-routing.module';
 import { FlexboxComponent } from './flexbox.component';
+import { NavigationModule } from '../navigation/navigation.module';
 
 @NgModule({
   declarations: [FlexboxComponent],
   imports: [
     CommonModule,
-    FlexboxRoutingModule
+    FlexboxRoutingModule,
+    NavigationModule
   ]
 })
 export class FlexboxModule { }
