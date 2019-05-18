ed13eb57d169ee01b061a7052844528fd3803323
Erik Tallang <e.tallang@gmail.com>
2019-05-18 13:21:52 +0200

    feat(margin): add initial slides

--- src/app/margin-vs-padding/margin-vs-padding.component.ts
+++ src/app/margin-vs-padding/margin-vs-padding.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+
+import { SlideDirective } from '../navigation/slide.directive';
+import { NavigationManager } from '../navigation/navigation-manager';
 
 @Component({
   selector: 'app-margin-vs-padding',
   templateUrl: './margin-vs-padding.component.html',
   styleUrls: ['./margin-vs-padding.component.css']
 })
-export class MarginVsPaddingComponent implements OnInit {
+export class MarginVsPaddingComponent implements AfterViewInit {
+  @ViewChildren(SlideDirective) elements: QueryList<SlideDirective>;
+  constructor(private navigationManager: NavigationManager) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.navigationManager.setAnchorItems(this.elements);
   }
-
 }
--- src/app/margin-vs-padding/margin-vs-padding.module.ts
+++ src/app/margin-vs-padding/margin-vs-padding.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MarginVsPaddingRoutingModule } from './margin-vs-padding-routing.module';
 import { MarginVsPaddingComponent } from './margin-vs-padding.component';
+import { NavigationModule } from '../navigation/navigation.module';
 
 @NgModule({
   declarations: [MarginVsPaddingComponent],
   imports: [
     CommonModule,
-    MarginVsPaddingRoutingModule
+    MarginVsPaddingRoutingModule,
+    NavigationModule
   ]
 })
 export class MarginVsPaddingModule { }
